c7ca1966d2c9ec36251c9f85bbe0edd4c873eec0
Walter Satriano <walter.satri@gmail.com>
2019-10-04 12:43:43 +0200

    definizione rotte

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/postIndex", "ControllerPost@index") -> name("post.index");
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/postCreate", "ControllerPost@create") -> name("post.create");
+
+Route::post("/", "ControllerPost@store") -> name("post.store");
+
+Route::get("/{id}/edit", "ControllerPost@edit") -> name("post.edit");
+Route::post("/{id}", "ControllerPost@update") -> name("post.update");
+
+Route::get("/{id}/delete", "ControllerPost@destroy") ->name("post.destroy");
